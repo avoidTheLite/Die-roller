87a72b7af77c27ed902b175f7956f058f36226f7
avoidTheLite <metalsupremacist@gmail.com>
2023-06-24 13:11:57 -0400

    Revert "first version of service code - untested HTTP headers, but but can hit the route from a browser."
    This reverts commit 32b86cec6b7a8a87714019755a642b4df1641051.

--- server/modules/rollMath.js
+++
@@ -1,9 +0,0 @@
-
-
-export function rollNSided(sides) {
-    let inputSides = sides
-    console.log('rollNSided, inputSides = ' + inputSides);
-    let rollResult = Math.ceil(inputSides*Math.random());
-return rollResult
-}
-
